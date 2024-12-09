f8df1a0ae1d204b85f23d92905c2eab4769ed152
PhoenixDSteele <phoenixdsteele@gmail.com>
2024-12-09 17:34:42 -0600

    Changed Player Controller, Added Export Settings and Lerping Movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,33 +1,27 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export_group("Movement Settings")
+@export var move_speed:float = 5
+@export var acceleration:float = 4
+@export var decceleration:float = 5
+@export var rotation_speed:float = 10
 
 @onready var body: MeshInstance3D = $Body
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	print(input_dir)
 	var direction := transform.basis * Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, $CamRoot/SpringArm3D.rotation.y).normalized()
 	var target_rotation = atan2(direction.x, direction.z) - rotation.y
+	
 	if direction:
-		body.rotation.y = lerp_angle(body.rotation.y, target_rotation, 20 * delta)
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		body.rotation.y = lerp_angle(body.rotation.y, target_rotation, rotation_speed * delta)
+		velocity.x = lerp(velocity.x, direction.x * move_speed, acceleration * delta)
+		velocity.z = lerp(velocity.z, direction.z * move_speed, acceleration * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = lerp(velocity.x, 0.0, decceleration * delta)
+		velocity.z = lerp(velocity.z, 0.0, decceleration * delta)
 
 	move_and_slide()
