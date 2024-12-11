35f913877dd93f36fdd3ab69af7eced32de63af7
PhoenixDSteele <phoenixdsteele@gmail.com>
2024-12-11 13:39:56 -0600

    Fix: Gravity added to enemy, to stop them from flying slightly upward while moving.

--- scripts/enemies/enemy_base.gd
+++ scripts/enemies/enemy_base.gd
@@ -1,5 +1,8 @@
 extends CharacterBody3D
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
 	move_and_slide()
