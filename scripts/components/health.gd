a7da1d9ce16c294a4401e8efbb7ca7ff4180e949
Mox <977898+MoxieWhimsy@users.noreply.github.com>
2024-12-11 17:55:42 -0500

    Add simple health component

---
+++ scripts/components/health.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@export var maximum:int = 3
+
+var current:int
+
+signal changed(health:int, delta:int)
+
+func _ready() -> void:
+	current = maximum
+
+func take_damage(damage_amount:int) -> void:
+	var overkill = maxi(damage_amount - current, 0)
+	damage_amount = mini(damage_amount, current)
+	if damage_amount <= 0: return
+	current -= damage_amount
+	changed.emit(current, -damage_amount)
