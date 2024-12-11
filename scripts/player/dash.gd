a45ce9be61306ed7e2ec9b85f424f0a62981fef2
Mox <977898+MoxieWhimsy@users.noreply.github.com>
2024-12-11 15:32:24 -0500

    Player can Dodge/Dash/Roll past enemies

--- scripts/player/dash.gd
+++ scripts/player/dash.gd
@@ -4,7 +4,11 @@ extends CharacterState
 
 @export var distance:float = 5.0
 @export var duration:float = 0.2
+@export_flags_2d_physics var dash_layer:int
+@export_flags_2d_physics var dash_mask:int = 0x1
 
+var default_layer:int
+var default_mask:int
 var speed:float
 var timer:Timer
 
@@ -15,14 +19,21 @@ func _ready() -> void:
 	timer.one_shot = true
 	timer.timeout.connect(done)
 	add_child(timer)
+	
+	default_layer = character.collision_layer
+	default_mask = character.collision_mask
 
 func enter(previous_state_path: String, data := {}) -> void:
 	speed = distance / duration
 	dash_dir = data.vector
 	timer.start(duration)
 	character.set_orientation_from_top_down_vector(dash_dir)
+	character.collision_layer = dash_layer
+	character.collision_mask = dash_mask
 	
 func exit() -> void:
+	character.collision_layer = default_layer
+	character.collision_mask = default_mask
 	character.velocity.x = 0
 	character.velocity.z = 0
 
