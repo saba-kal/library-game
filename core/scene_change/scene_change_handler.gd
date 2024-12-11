095e84c010d6a27c2e9dcf6af79afbd192e72577
PhoenixDSteele <phoenixdsteele@gmail.com>
2024-12-11 09:18:54 -0600

    Move: Moved states folder for enemies to /enemies, to not get them mixed up with other state machines.

--- core/scene_change/scene_change_handler.gd
+++ core/scene_change/scene_change_handler.gd
@@ -11,30 +11,30 @@ var next_scene:PackedScene
 var current_scene:PackedScene
 
 func to_main_menu() -> void:
-    next_scene = main_menu
-    current_scene = main_menu
-    scene_change()
+	next_scene = main_menu
+	current_scene = main_menu
+	scene_change()
 
 func to_debug_area() -> void:
-    next_scene = debug_area
-    current_scene = debug_area
-    scene_change()
+	next_scene = debug_area
+	current_scene = debug_area
+	scene_change()
 
 func to_debug_generation_area() -> void:
-    next_scene = debug_generation_area
-    current_scene = debug_generation_area
-    scene_change()
+	next_scene = debug_generation_area
+	current_scene = debug_generation_area
+	scene_change()
 
 
 func scene_change() -> void:
-    if timer.is_stopped():
-        load_cover.visible = true
-        Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
-        timer.start(scene_change_duration)
-    else:
-        return
+	if timer.is_stopped():
+		load_cover.visible = true
+		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+		timer.start(scene_change_duration)
+	else:
+		return
 
 func scene_change_finished() -> void:
-    load_cover.visible = false
-    Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-    get_tree().change_scene_to_packed(next_scene)
+	load_cover.visible = false
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	get_tree().change_scene_to_packed(next_scene)
--- scripts/main_menu/main_menu.gd
+++ scripts/main_menu/main_menu.gd
@@ -2,17 +2,17 @@ extends Node
 
 
 func play_pressed() -> void:
-    print("Pressed Play Button")
+	print("Pressed Play Button")
 
 func exit_pressed() -> void:
-    print("Pressed Exit Button")
-    get_tree().quit()
+	print("Pressed Exit Button")
+	get_tree().quit()
 
 func debug_pressed() -> void:
-    print("Pressed Debug Button")
-    ChangeScene.to_debug_area()
+	print("Pressed Debug Button")
+	ChangeScene.to_debug_area()
 
 
 func debug_generation_pressed():
-    print("Pressed Debug Generation Button")
-    ChangeScene.to_debug_generation_area()
+	print("Pressed Debug Generation Button")
+	ChangeScene.to_debug_generation_area()
