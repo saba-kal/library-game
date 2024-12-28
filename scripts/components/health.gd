96c1a04759ef77e2bc9d8067ed9457a0d18133a8
saba-kal <38592380+saba-kal@users.noreply.github.com>
2024-12-28 15:15:14 -0500

    Added heal collectible

--- scripts/components/health.gd
+++ scripts/components/health.gd
@@ -10,9 +10,15 @@ func _ready() -> void:
 	current = maximum
 	owner.set_meta("damageable", true)
 
+
 func take_damage(damage_amount:int) -> void:
 	var overkill = maxi(damage_amount - current, 0)
 	damage_amount = mini(damage_amount, current)
 	if damage_amount <= 0: return
 	current -= damage_amount
 	changed.emit(current, -damage_amount)
+
+
+func heal(heal_amount: int) -> void:
+	current = clamp(self.current + heal_amount, 0, self.maximum)
+	self.changed.emit(self.current, heal_amount)
