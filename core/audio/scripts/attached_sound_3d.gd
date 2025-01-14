d6bb2630ea68e47f59e47c08829a1208bf3fd69b
PhoenixDSteele <phoenixdsteele@gmail.com>
2025-01-13 21:42:44 -0600

    Refactored footsteps to be played/called on frames of the run animation.

--- core/audio/scripts/attached_sound_3d.gd
+++ core/audio/scripts/attached_sound_3d.gd
@@ -13,6 +13,7 @@ func _ready() -> void:
 
 func play() -> void:
 	self.audio_stream_player_3d.play()
+	print(audio_stream_player_3d.pitch_scale)
 
 
 func stop():
--- core/audio/scripts/footstep_sounds.gd
+++ core/audio/scripts/footstep_sounds.gd
@@ -1,30 +1,21 @@
 class_name FootstepSounds extends AttachedSound3D
 
-@export_range(0.01, 5) var interval: float = 0.5
-@export_range(0, 4) var pitch_randomness: float = 0
+@export_range(0, 2, 0.1) var pitch_randomness: float = 0
 
-var time_since_last_play: float = 0
-var enabled: bool = false
 var original_pitch_scale: float
 
 
 func _ready() -> void:
 	super._ready()
-	self.original_pitch_scale = self.audio_stream_player_3d.pitch_scale
-
+	original_pitch_scale = audio_stream_player_3d.pitch_scale
 
 func _process(delta: float) -> void:
-	if self.time_since_last_play >= self.interval && enabled:
-		self.audio_stream_player_3d.pitch_scale = self.original_pitch_scale + randf_range(-self.pitch_randomness, self.pitch_randomness)
-		self.audio_stream_player_3d.play()
-		self.time_since_last_play = 0
-	else:
-		self.time_since_last_play += delta
-
+	pass
 
 func play() -> void:
-	self.enabled = true
-
+	audio_stream_player_3d.pitch_scale += randf_range(-pitch_randomness, pitch_randomness) * .1
+	super.play()
+	audio_stream_player_3d.pitch_scale = original_pitch_scale
 
 func stop():
-	self.enabled = false
+	pass
