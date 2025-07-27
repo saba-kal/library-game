66cc1487d89d09862a617f8376786165772454d2
saba-kal <38592380+saba-kal@users.noreply.github.com>
2025-07-27 16:57:12 -0400

    Further buffs to wolf and witch

--- scripts/enemies/states/EnemyHowl.gd
+++ scripts/enemies/states/EnemyHowl.gd
@@ -3,6 +3,7 @@ extends State
 @export var howl_anim_name: String
 @export var howl_sound:AttachedSound3D
 @export var chase_state:String = ""
+@export var howl_speed:float = 1.0
 @onready var howl_timer = $"Howl timer"
 @onready var end_howl_timer = $"End-howl timer"
 
@@ -13,12 +14,12 @@ func _ready():
 func Enter():
 	self.nav_agent.is_disabled = true
 	self.enemy.velocity = Vector3.ZERO
-	anim_player.play(howl_anim_name)
-	howl_timer.start()
+	anim_player.play(howl_anim_name, -1, howl_speed)
+	howl_timer.start(howl_timer.wait_time / howl_speed)
 
 func howl():
 	enemy.howl.emit()
-	end_howl_timer.start()
+	end_howl_timer.start(end_howl_timer.wait_time / howl_speed)
 	if howl_sound:
 		howl_sound.play()
 
